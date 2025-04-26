db929abe8dd6218cb175778c50cf57ac32ca041e
Shamar Malcolm <malco0253@gmail.com>
2025-04-26 13:57:19 -0500

    Added logout route, get all courses route, get student courses route and get courses lecturers teach route

--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import InputRequired
+from db import connectDB
 
 
 class LoginForm(FlaskForm):
@@ -10,7 +11,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Login')
 
 class RegisterForm(FlaskForm):
-    user_id = StringField('Username', validators=[InputRequired()])
+    user_id = StringField('User_ID', validators=[InputRequired()])
     first_name = StringField('First_Name', validators=[InputRequired()])
     last_name = StringField('Last_Name', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
@@ -18,8 +19,30 @@ class RegisterForm(FlaskForm):
     submit = SubmitField('Register')
 
 
+class CourseForm(FlaskForm):
+    def get_lecturers():
+        try:
+            db_connect = connectDB()
+            cursor= db_connect.cursor()
+            lecturer_query = "SELECT LFirstName, LLastName FROM CMS_Lecturers"
+            cursor.execute(lecturer_query)
+            lecturers = cursor.fetchall()
+            lecturer_lst = [("{0}", "{1}").format(lecturer[0], lecturer[1])for lecturer in lecturers]
+            return lecturer_lst
+        except Exception as err:
+            print({"Error": err})
+    pass
+
+    course_code =  StringField("Course_code", validators=[InputRequired()])
+    course_name = StringField("Course_name", validators=[InputRequired()])
+    department = SelectField("Department", choices=[("Science and Technology"), ('Social Sciences'), ('Medical Sciences')])
+    lecturer = SelectField("Lecturer", choices=[get_lecturers()])
+    submit = SubmitField("Create course")
+    pass
 
 
+
+    
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,35 +1,24 @@
 import os
 from app import app, login_manager
-from flask import Blueprint, render_template, request, redirect, jsonify,url_for
-from flask_login import login_user, login_required, logout_user
+from flask import Blueprint, render_template, request, redirect, jsonify,url_for,flash
+from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 from flask_bcrypt import Bcrypt 
-from app.forms import LoginForm, RegisterForm
-import pymysql
-import jwt, datetime
+from app.forms import LoginForm, RegisterForm, CourseForm
+import jwt, datetime,json
 from app.utils import token_required
-
-
+from db import connectDB
 
 app_views = Blueprint('app_views', __name__)
 bcrypt = Bcrypt(app) 
-
-def connectDB():
-    return pymysql.connect(
-        user='CMS_ADMIN', 
-        password= app.config['PASSWORD'], 
-        host='localhost', 
-        database='CourseManagementSystem',
-        auth_plugin='mysql_native_password'
-    )
 
 
 @app_views.route('/login', methods=['POST'])
 def user_login():
     form = LoginForm()
-    db = connectDB()
-    cursor = db.cursor()
+    db_connect = connectDB()
+    cursor = db_connect.cursor()
     if form.validate_on_submit():
         user_id = form.user_id.data
         user_password = form.password.data
@@ -54,15 +43,15 @@ def user_login():
 def user_register():
     try:
         form = RegisterForm()
-        db= connectDB()
-        cursor = db.cursor()
+        db_connect= connectDB()
+        cursor = db_connect.cursor()
         if form.validate_on_submit():
             user_id = form.user_id.data
             password = form.password.data
             first_name = form.first_name.data
             last_name = form.last_name.data
 
-            hashed_password = bcrypt.generate_password_hash('password').decode('utf-8') 
+            hashed_password = bcrypt.generate_password_hash(password).decode('utf-8') 
 
             acc_insert = "INSERT INTO CMS_Account (AccID, AccPassword) VALUES (%s, %s)"
             cursor.execute(acc_insert, (user_id, hashed_password ))
@@ -75,11 +64,134 @@ def user_register():
                 lecturer_insert = "INSERT INTO CMS_Lecturers(LecID,LFirstName,LLastname ,Department) VALUES (%s,%s,%s,%s)"
                 cursor.execute = (lecturer_insert, (user_id, first_name, last_name, request.form['Department']))
 
-            db.commit()
+            db_connect.commit()
             return redirect(url_for('user_login'))
         return render_template('signup.html', form=form)
     except Exception as err:
         print({"Error": err})
 
+#how to get the id for the account
+@login_required
+@app_views.route('/create/course', methods=['POST'])
+def create_course():
+    if request.form['role'] == 'Admin':
+        try:
+            form = CourseForm()
+            db_connect = connectDB()
+            cursor = db_connect.cursor()
 
+            if form.validate_on_submit():
+                course_code = form.course_code.data
+                course_name = form.course_name.data
+                department = form.department.data
+                lecturer_name = form.lecturer.data
+                def get_lecturer_id(name):
+                    db_connect = connectDB()
+                    cursor = db_connect.cursor()
+                    lec_query = "SELECT LecID FROM CMS_Lecturers WHERE LFirstName= AND    LLastname= "
+                    return id
+                
+                lecturer_id = get_lecturer_id(lecturer_name)
+
+                course_insert = "INSERT INTO CMS_Courses(CName,CCode,CDepartment, LecID) VALUES (%s, %s, %s, %s)"
+                cursor.execute(course_insert, (course_name, course_code, department, lecturer_id))
+
+                db_connect.commit()
+                return redirect(url_for('create_course'))
+
+                pass
+        except:
+            pass
+
+
+
+@login_required
+@app_views.route('/logout', methods=['POST'])
+def logout():
+    logout_user()
+    return redirect(url_for("user_login"))
+    
+
+#Retrieve Courses
+@app_views.route("/api/courses", methods=["GET"])
+def get_courses():
+    if request.method== 'GET':
+        try:
+            db_connect = connectDB()
+            cursor = db_connect.cursor()
+
+            courses_query = "SELECT * FROM CMS_Courses"
+            cursor.execute(courses_query)
+
+            course_lst = []
+
+            for course_id, course_name, course_code, course_department,lec_id in cursor:
+                course = {}
+                course['CID'] = course_id
+                course[' CName '] = course_name
+                course['CCode'] = course_code
+                course['CDepartment'] = course_department
+                course['LecID'] = lec_id
+
+                course_lst.append(course)
+            cursor.close()
+            db_connect.close()
+            return json.dumps(course_lst, sort_keys=False)
+        except Exception as err:
+            print({"Error": err})
+
+
+@app_views.route("/api/courses/<student_id>", methods=['POST'])
+def get_student_courses(student_id):
+    if request.method =='POST':
+        try:
+            db_connect = connectDB()
+            cursor = db_connect.cursor()
+
+            student_courses = "SELECT CID,CName,CCode,CDepartment,LecID  FROM CMS_Courses INNER JOIN CMS_Enrolment on  CMS_Courses.CID=CMS_Enrolment.CID WHERE CMS_Enrolment.StudID={0}".format(student_id)
+            cursor.execute(student_courses)
+
+            course_lst = []
+
+            for course_id, course_name, course_code, course_department,lec_id in cursor:
+                course = {}
+                course['CID'] = course_id
+                course[' CName '] = course_name
+                course['CCode'] = course_code
+                course['CDepartment'] = course_department
+                course['LecID'] = lec_id
+
+                course_lst.append(course)
+            cursor.close()
+            db_connect.close()
+            return json.dumps(course_lst, sort_keys=False)
+        except Exception as err:
+            print({"Error": err})        
+
+@app_views.route("/api/courses/<lecturer_id>", methods=['POST'])
+def get_lecturer_courses(lecturer_id):
+    if request.method =='POST':
+        try:
+            db_connect = connectDB()
+            cursor = db_connect.cursor()
+
+            courses_taught = "SELECT CID,CName,CCode,CDepartment,LecID FROM CMS_Courses WHERE LecID={0}".format(lecturer_id)
+            cursor.execute(courses_taught)
+
+            course_lst = []
+
+            for course_id, course_name, course_code, course_department,lec_id in cursor:
+                course = {}
+                course['CID'] = course_id
+                course['CName'] = course_name
+                course['CCode'] = course_code
+                course['CDepartment'] = course_department
+                course['LecID'] = lec_id
+
+                course_lst.append(course)
+            cursor.close()
+            db_connect.close()
+            return json.dumps(course_lst, sort_keys=False)
+        except Exception as err:
+            print({"Error": err})  
 
